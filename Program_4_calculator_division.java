21b3addcf2700c4c47b0442ee62a59df296c4042
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2021-11-27 18:14:09 +0530

    Update Program_4_calculator_division.java

--- Program_4_calculator_division.java
+++ Program_4_calculator_division.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Program_4_calculator_division {
     public static void main(String[] args){
-        System.out.println("Substraction");
+        System.out.println("Division");
         System.out.println("Enter the first number");
         Scanner num1 = new Scanner(System.in);
         int writenum1 = num1.nextInt();
